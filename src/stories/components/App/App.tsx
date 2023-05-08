3cbed2ada126d461cd82d62c7d18923fdf34eee8
Amanda Iaria <amanda.iaria@velir.com>
2023-05-08 16:45:01 -0400

    Vite-start: Finish setting up tests and tokens to be used in storybook

--- src/stories/components/App/App.tsx
+++ src/stories/components/App/App.tsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
-import reactLogo from '../assets/react.svg'
+import reactLogo from '../../../assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.scss'
+import {Header} from '../Header/Header.tsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Header
+        onCreateAccount={() => {}}
+        onLogin={() => {}}
+        onLogout={() => {}}
+      />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
